7fe9f58fddcb1f9cbf3d6dc9165e621688739b55
Soffi Filianto <sfzerox@gmail.com>
2018-07-09 10:15:34 +0700

    second commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,16 @@
+// Sidebar Laptop Show
 var slide_navbar = document.querySelector('.sidebar-toggler'),
     body_navbar = document.body.classList;
 
 slide_navbar.addEventListener('click', _ => {
     body_navbar.toggle('sidebar-lg-show');
 });
+
+// Responsive
+var mq = "(max-width: 991.98px)";
+var navbar_mobile = document.querySelector("#navbarResponsive");
+if (window.matchMedia(mq.matches)) {
+    navbar_mobile.classList.remove("show");
+} else {
+    navbar_mobile.classList.add("show");
+}
